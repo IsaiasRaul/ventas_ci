001be89e5b130dcebfd846aee46dd4b469834519
Isaias <isaias.cerda.murillo@gmail.com>
2020-10-23 12:38:11 -0300

    cambios en la configuracion

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'http://localhost/ventas_ci/';
-$config['base_url'] = 'http://'.$_SERVER["SERVER_NAME"].'/ventas_ci/';
+$config['base_url'] = 'http://'.$_SERVER["SERVER_NAME"].'/ventas/';
 
 /*
 |--------------------------------------------------------------------------
